39d73b3847ca06d07945dea6af29794fac604a3b
Jaime Gonzalez <lnsflive@gmail.com>
2023-03-15 22:37:22 -0500

    update

--- server/server.js
+++ server/server.js
@@ -3,9 +3,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true }, (err) =>
-  console.log(err)
-);
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 
 app.listen(4444, () => console.log(`connected at 4444`));
 
