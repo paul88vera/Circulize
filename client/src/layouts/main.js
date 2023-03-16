dd3b11f0f9a3b71a6d573d6dcd7aa4ffade1c63c
Jaime Gonzalez <lnsflive@gmail.com>
2023-03-16 14:00:16 -0500

    layout cleanup

---
+++ client/src/layouts/main.js
@@ -0,0 +1,46 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import Banner from '../components/Banner';
+import Navbar from '../components/Navbar';
+import Home from '../pages/home';
+import Browse from '../pages/browse';
+import Login from '../pages/login';
+import Signup from '../pages/signup';
+import Splash from '../pages/splash';
+import Error from '../pages/error'
+import RestaurantPage from '../props/selection';
+
+
+
+export default function Layout() {
+    const [selectionData, setSelectionData] = useState()
+    useEffect(() => {
+        const fetchRestaurants = async () => {
+            fetch('http://localhost:4444/restaurants').then(res => res.json()).then(data => setSelectionData(data))
+        }
+        fetchRestaurants()
+    }, [])
+
+    if (selectionData) {
+        return (
+            <BrowserRouter>
+                <Navbar />
+                <Banner />
+                <Routes>
+                    <Route index path="/" element={<Home />} />
+                    <Route path='/search' element={<Splash />} />
+                    <Route path="SanAntonio/Mexican" element={<RestaurantPage title="Mexican" data={selectionData} />} />
+                    <Route path="SanAntonio/Seafood" element={<RestaurantPage title="Seafood" data={selectionData} />} />
+                    <Route path="SanAntonio/Italian" element={<RestaurantPage title="Italian" data={selectionData} />} />
+                    <Route path="SanAntonio/American" element={<RestaurantPage title="American" data={selectionData} />} />
+                    <Route path="SanAntonio/Asian" element={<RestaurantPage title="Asian" data={selectionData} />} />
+                    <Route path="SanAntonio/Barbecue" element={<RestaurantPage title="Barbecue" data={selectionData} />} />
+                    <Route path="browse" element={<Browse />} />
+                    <Route path="login" element={<Login />} />
+                    <Route path='signup' element={<Signup />} />
+                    <Route path='*' element={<Error />} />
+                </Routes>
+            </BrowserRouter>
+        );
+    }
+}
--- client/src/props/selection/index.js
+++ client/src/props/selection/index.js
@@ -2,7 +2,6 @@ import { useState } from "react";
 import Restaurant from "../restaurant";
 
 export default function Selection(props) {
-  const selection = props.selection;
 
   const [openModal, setOpenModal] = useState(false);
 
@@ -14,28 +13,32 @@ export default function Selection(props) {
   }
 
   return (
-    <section id="cuisine-card-container">
-      {selection.map((sel) => (
-        <div
-          className="cuisine-card"
-          key={sel._id}
-          style={{ cursor: "pointer" }}
-          onClick={() => handleClick(sel)}
-        >
-          <div className="cuisine-card-img">
-            <img src={sel.img} alt={sel.alt} width="100" />
-          </div>
-          <div className="cuisine-card-text">
-            <h4>{sel.city}</h4>
-            <p>{sel.cuisine}</p>
-            <h3>{sel.name}</h3>
-          </div>
-        </div>
-      ))}
 
-      {openModal && (
-        <Restaurant data={restaurantData} closeModal={setOpenModal} />
-      )}
-    </section>
+    <div className="cuisine-gallery">
+      <h4 className="cuisine-title">{props.title}</h4>
+      <section id="cuisine-card-container">
+        {props.data.map((sel) => (
+          <div
+            className="cuisine-card"
+            key={sel._id}
+            style={{ cursor: "pointer" }}
+            onClick={() => handleClick(sel)}
+          >
+            <div className="cuisine-card-img">
+              <img src={sel.img} alt={sel.alt} width="100" />
+            </div>
+            <div className="cuisine-card-text">
+              <h4>{sel.city}</h4>
+              <p>{sel.cuisine}</p>
+              <h3>{sel.name}</h3>
+            </div>
+          </div>
+        ))}
+
+        {openModal && (
+          <Restaurant data={restaurantData} closeModal={setOpenModal} />
+        )}
+      </section>
+    </div>
   );
 }
