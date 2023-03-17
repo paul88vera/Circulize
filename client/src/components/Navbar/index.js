0bca88111e2e6fb3d2874bba5b89663773427f17
Jaime Gonzalez <lnsflive@gmail.com>
2023-03-16 22:20:35 -0500

    tailwind

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div id="navbar">
+    <div className="flex flex-row flex-nowrap rounded-bl-3xl rounded-br-3xl bg-green-900 justify-between absolute w-full">
       <Link to="/" ><i className="fa-solid fa-circle-chevron-left"></i></Link>
       <Link to="/search"><i className="fa-brands fa-gg-circle"></i></Link>
       <div id="burger">
--- client/src/layouts/main.js
+++ client/src/layouts/main.js
@@ -25,23 +25,25 @@ export default function Layout() {
     if (selectionData) {
         return (
             <BrowserRouter>
-                <Navbar />
-                <Banner />
-                <Routes>
-                    <Route index path="/" element={<Home />} />
-                    <Route path='/search' element={<Splash />} />
-                    <Route path='/add' element={<AddRestaurant />} />
-                    <Route path="SanAntonio/Mexican" element={<RestaurantPage title="Mexican" data={selectionData} />} />
-                    <Route path="SanAntonio/Seafood" element={<RestaurantPage title="Seafood" data={selectionData} />} />
-                    <Route path="SanAntonio/Italian" element={<RestaurantPage title="Italian" data={selectionData} />} />
-                    <Route path="SanAntonio/American" element={<RestaurantPage title="American" data={selectionData} />} />
-                    <Route path="SanAntonio/Asian" element={<RestaurantPage title="Asian" data={selectionData} />} />
-                    <Route path="SanAntonio/Barbecue" element={<RestaurantPage title="Barbecue" data={selectionData} />} />
-                    <Route path="browse" element={<Browse />} />
-                    <Route path="login" element={<Login />} />
-                    <Route path='signup' element={<Signup />} />
-                    <Route path='*' element={<Error />} />
-                </Routes>
+                <div className='h-screen w-screen'>
+                    <Navbar />
+                    <Banner />
+                    <Routes>
+                        <Route index path="/" element={<Home />} />
+                        <Route path='/search' element={<Splash />} />
+                        <Route path='/add' element={<AddRestaurant />} />
+                        <Route path="SanAntonio/Mexican" element={<RestaurantPage title="Mexican" data={selectionData} />} />
+                        <Route path="SanAntonio/Seafood" element={<RestaurantPage title="Seafood" data={selectionData} />} />
+                        <Route path="SanAntonio/Italian" element={<RestaurantPage title="Italian" data={selectionData} />} />
+                        <Route path="SanAntonio/American" element={<RestaurantPage title="American" data={selectionData} />} />
+                        <Route path="SanAntonio/Asian" element={<RestaurantPage title="Asian" data={selectionData} />} />
+                        <Route path="SanAntonio/Barbecue" element={<RestaurantPage title="Barbecue" data={selectionData} />} />
+                        <Route path="browse" element={<Browse />} />
+                        <Route path="login" element={<Login />} />
+                        <Route path='signup' element={<Signup />} />
+                        <Route path='*' element={<Error />} />
+                    </Routes>
+                </div>
             </BrowserRouter>
         );
     }
